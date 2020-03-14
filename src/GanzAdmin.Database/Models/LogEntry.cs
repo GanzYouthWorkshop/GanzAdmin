927dce7b0f6c562b4b81dde1adb9e29e5791fbe3
Kimmel Gábor <kimmelgabor@gmail.com>
2020-03-14 15:35:34 +0100

    Started user project module

---
+++ src/GanzAdmin.Database/Models/LogEntry.cs
@@ -0,0 +1,27 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GanzAdmin.Database.Models
+{
+    public class LogEntry
+    {
+        [BsonId]
+        public long Id { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public string Content { get; set; }
+
+        [BsonRef]
+        public Member Writer { get; set; }
+
+
+        //TODO
+        [BsonIgnore]
+        public string DisplayValue
+        {
+            get { return ""; }
+        }
+    }
+}
---
+++ src/GanzAdmin.Database/Models/MemberProject.cs
@@ -0,0 +1,30 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GanzAdmin.Database.Models
+{
+    public class MemberProject : IEntity
+    {
+        [BsonId]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsFinished { get; set; }
+        public DateTime StartTime { get; set; }
+        public string Logo { get; set; }
+
+        [BsonRef]
+        public List<Member> Members { get; set; }
+
+        public List<LogEntry> ProjectLog { get; set; }
+
+        [BsonIgnore]
+        public string DisplayValue
+        {
+            get { return this.Name; }
+        }
+    }
+}
