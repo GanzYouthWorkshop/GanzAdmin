bf1398e8e7fc11edb044e44aaa3e30ae5289b9a7
Kimmel Gábor <kimmelgabor@gmail.com>
2020-03-30 11:38:20 +0200

    Tweaked admin tool

--- src/GanzAdmin.ServerAdministration/ucAdminTask.cs
+++ src/GanzAdmin.ServerAdministration/ucAdminTask.cs
@@ -29,17 +29,28 @@ namespace GanzAdmin.ServerAdministration
 
         public void Run()
         {
-            this.RunInternal();
-            this.LastRun = DateTime.Now;
-            this.lblLastRun.Text = this.LastRun.ToString("yyyy.MM.dd. hh:mm:ss");
-            this.NextRun = this.LastRun + this.RunInterval;
+            this.btnRunNow.Enabled = false;
+            Task.Run(new Action(() =>
+            {
+                this.RunInternal();
+                this.Invoke(new Action(this.AfterRun));
+            }));
         }
 
         protected abstract void RunInternal();
 
+        protected void AfterRun()
+        {
+            this.LastRun = DateTime.Now;
+            this.lblLastRun.Text = this.LastRun.ToString("yyyy.MM.dd. hh:mm:ss");
+            this.NextRun = this.LastRun + this.RunInterval;
+            this.btnRunNow.Enabled = true;
+        }
+
         private void btnRunNow_Click(object sender, EventArgs e)
         {
             this.NextRun = DateTime.Now;
+            this.btnRunNow.Enabled = false;
         }
     }
 }
