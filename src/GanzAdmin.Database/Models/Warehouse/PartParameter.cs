27b9756934e531f27970b70f5558370e206905b4
Kimmel Gábor <kimmelgabor@gmail.com>
2020-04-16 17:51:45 +0200

    Tags editor and member projects finally work

--- src/GanzAdmin.Database/Models/Warehouse/PartParameter.cs
+++ src/GanzAdmin.Database/Models/Warehouse/PartParameter.cs
@@ -7,7 +7,16 @@ namespace GanzAdmin.Database.Models
     public class PartParameter
     {
         public string Name { get; set; }
+        public string DefaultValue { get; set; }
         public string Value { get; set; }
         public string Unit { get; set; }
+
+        public bool IsSelectValue
+        {
+            get
+            {
+                return (bool)this.DefaultValue?.Contains("|");
+            }
+        }
     }
 }
