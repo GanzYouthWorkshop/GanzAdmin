e8620b7205843e67afeebe86f9e9ae11b4a356e4
Szlatyka <kimmelgabor@gmail.com>
2020-06-20 22:25:56 +0200

    Normalized parts handling #2

--- src/GanzAdmin.Database/Models/Warehouse/Part.cs
+++ src/GanzAdmin.Database/Models/Warehouse/Part.cs
@@ -16,16 +16,15 @@ namespace GanzAdmin.Database.Models
         public Category Category { get; set; }
 
         public List<Stockpile> Stock { get; set; } = new List<Stockpile>();
-
         public int MinimumStock { get; set; } = 0;
+        public string StockUnit { get; set; }
 
         public string ShortDescription { get; set; }
-
         public string LongDescription { get; set; }
-
         public string ImagePath { get; set; }
-
         public string DateSheet { get; set; }
+
+        public List<PartParameter> Parameters { get; set; } = new List<PartParameter>();
 
 
         [BsonIgnore]
