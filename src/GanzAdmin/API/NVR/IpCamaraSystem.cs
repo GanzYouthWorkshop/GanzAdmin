24f098af8c19c4c9eec874e9d15b907a7ae57553
Szlatyka <kimmelgabor@gmail.com>
2020-06-20 20:48:01 +0200

    Normalized parts handling

---
+++ src/GanzAdmin/API/NVR/IpCamaraSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GanzAdmin.API.NVR
+{
+    public class IpCamaraSystem
+    {
+        public List<IpCameraChannel> Channels { get; set; } = new List<IpCameraChannel>();
+
+        public IpCamaraSystem(int channels)
+        {
+            for(int i = 0; i < channels; i++)
+            {
+                this.Channels.Add(new IpCameraChannel() { ChannelId = i + 1 });
+            }
+        }
+
+        public void Open(string ip)
+        {
+            foreach(IpCameraChannel channel in this.Channels)
+            {
+                channel.Start(ip);
+            }
+        }
+    }
+}
---
+++ src/GanzAdmin/API/NVR/IpCameraChannel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GanzAdmin.API.NVR
+{
+    public class IpCameraChannel
+    {
+        public bool Runnning
+        {
+            get
+            {
+                return false; // this.m_FfmpegProcess?.Responding && !this.m_FfmpegProcess.HasExited;
+            }
+        }
+        public int ChannelId { get; set; }
+
+        private Process m_FfmpegProcess;
+
+        public void Start(string ip)
+        {
+            if(!this.Runnning)
+            {
+                this.m_FfmpegProcess = new Process
+                {
+                    StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = ".\\ffmpeg.exe",
+                        Arguments = "",
+                        CreateNoWindow = true,
+                    }
+                };
+                this.m_FfmpegProcess.Start();
+            }
+        }
+    }
+}
