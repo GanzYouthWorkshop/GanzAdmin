1cf04802a7495c459d69fb77ac47007bbd591cb9
Szlatyka <kimmelgabor@gmail.com>
2020-06-21 21:15:46 +0200

    Supplier functionality added

---
+++ src/GanzAdmin.Database/Models/Supplies/Supplier.cs
@@ -0,0 +1,28 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GanzAdmin.Database.Models
+{
+    public class Supplier : IEntity
+    {
+        [BsonId]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+        public string Website { get; set; }
+        public string ItemLinkTemplate { get; set; }
+
+        public string DisplayValue
+        {
+            get { return this.Name; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.IsEqual(obj);
+        }
+
+    }
+}
