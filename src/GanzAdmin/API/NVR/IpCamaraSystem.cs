9a71f7d70b3e5599dab0b71abfd74dd4de6913ba
Szlatyka <kimmelgabor@gmail.com>
2020-06-23 19:02:43 +0200

    Skeletal developments

--- src/GanzAdmin/API/NVR/IpCamaraSystem.cs
+++ src/GanzAdmin/API/NVR/IpCamaraSystem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,13 +8,20 @@ namespace GanzAdmin.API.NVR
 {
     public class IpCamaraSystem
     {
+        public string Name { get; set; }
+        public string Url { get; set; } = "rtsp://192.168.1.20:10554/user=admin_password=tlJwpbo6_channel=1_stream=0.sdp";
         public List<IpCameraChannel> Channels { get; set; } = new List<IpCameraChannel>();
 
         public IpCamaraSystem(int channels)
         {
             for(int i = 0; i < channels; i++)
             {
-                this.Channels.Add(new IpCameraChannel() { ChannelId = i + 1 });
+                this.Channels.Add(new IpCameraChannel
+                {
+                    HandlerName = this.Name,
+                    HandlerUrl = this.Url,
+                    ChannelId = i + 1
+                });
             }
         }
 
@@ -23,6 +31,10 @@ namespace GanzAdmin.API.NVR
             {
                 channel.Start(ip);
             }
+
+            //<video id="video-player" controls preload="none">
+            // < source src = "/stream/index.m3u8" type = "application/x-mpegURL" >
+            //</ video >
         }
     }
 }
--- src/GanzAdmin/API/NVR/IpCameraChannel.cs
+++ src/GanzAdmin/API/NVR/IpCameraChannel.cs
@@ -12,9 +12,12 @@ namespace GanzAdmin.API.NVR
         {
             get
             {
-                return false; // this.m_FfmpegProcess?.Responding && !this.m_FfmpegProcess.HasExited;
+                return this.m_FfmpegProcess != null && this.m_FfmpegProcess.Responding && !this.m_FfmpegProcess.HasExited;
             }
         }
+
+        public string HandlerName { get; set; }
+        public string HandlerUrl { get; set; }
         public int ChannelId { get; set; }
 
         private Process m_FfmpegProcess;
@@ -28,7 +31,29 @@ namespace GanzAdmin.API.NVR
                     StartInfo = new ProcessStartInfo()
                     {
                         FileName = ".\\ffmpeg.exe",
-                        Arguments = "",
+                        Arguments = String.Join(' ', new string[]
+                        {
+                             "-fflags nobuffer",
+                             "-rtsp_transport tcp",
+
+                             $"-i {this.HandlerUrl}",
+
+                             "-vsync 0",
+                             "-copyts",
+                             "-vcodec copy",
+                             "-movflags frag_keyframe+empty_moov",
+                             "-an",
+                             "-hls_flags delete_segments+append_list",
+                             "-f segment",
+                             "-segment_list_flags live",
+                             "-segment_time 1",
+                             "-segment_list_size 3",
+                             "-segment_format mpegts",
+                             @$"-segment_list .\wwwroot\content\nvr\{this.HandlerName}_{this.ChannelId}.m3u8",
+                             "-segment_list_type m3u8",
+                             "-segment_list_entry_prefix /stream/",
+                             @$".\wwwroot\content\nvr\{this.HandlerName}_{this.ChannelId}_%d.ts",
+                        }),
                         CreateNoWindow = true,
                     }
                 };
