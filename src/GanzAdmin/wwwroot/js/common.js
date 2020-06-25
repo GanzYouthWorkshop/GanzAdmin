3a4d29fe821c2f1b88b1666ae5b790051d2d32c5
Szlatyka <kimmelgabor@gmail.com>
2020-06-25 18:11:54 +0200

    Fixed top popup menu

--- src/GanzAdmin/wwwroot/js/common.js
+++ src/GanzAdmin/wwwroot/js/common.js
@@ -1,19 +1,21 @@
 ﻿function onBlazorReady()
 {
-	$('header nav #menu').click(function (e)
-	{
-		e.stopPropagation();
-		$('header nav ul').toggle();
-	});
-
-	$('header, main, aside').click(function()
-	{
-		$('header nav > ul').hide();
-	});
 
 	alertify.init();
 
 	alertify.alert("A GanzAdmin még erősen fejlesztés alatt áll! Hogy pontosan mik a tervek és most éppen hogy áll a projekt, a Súgóban tudod megnézni!");
+}
+
+function onMenuReady()
+{
+	$('header nav #menu').click(function (e) {
+		e.stopPropagation();
+		$('header nav ul').toggle();
+	});
+
+	$('header, main, aside').click(function () {
+		$('header nav > ul').hide();
+	});
 }
 
 function onBlazorRender()
