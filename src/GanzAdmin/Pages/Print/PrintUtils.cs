794fb35d1f49620e611c2c8c3e694dc61b80b62d
Szlatyka <kimmelgabor@gmail.com>
2020-06-28 13:34:58 +0200

    Label print tweaked

---
+++ src/GanzAdmin/Pages/Print/PrintUtils.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GanzAdmin.Pages.Print
+{
+    public class PrintUtils
+    {
+        public static void PrintContent(IJSRuntime runtime, string content)
+        {
+            string formData = $"{{ \"print\": \"{content}\" }}";
+            runtime.InvokeVoidAsync("createPost", "./print", formData);
+        }
+    }
+}
