dcca56ad40b04d983e4cd27b1f9b4b3b1b9cfbda
Szlatyka <kimmelgabor@gmail.com>
2020-06-30 09:35:33 +0200

    Falstad embed almost works

--- src/GanzAdmin/wwwroot/js/common.js
+++ src/GanzAdmin/wwwroot/js/common.js
@@ -87,27 +87,30 @@ function createEditorJS(initialData, callbackObj)
 					inlineToolbar: itb,
 					config:
 					{
-						youtube: true,
-						coub: true,
-						falstad:
+						services:
 						{
-							regex: /http.*(falstad).*/,
-							embedUrl: 'https://www.falstad.com/circuit/circuitjs.html<%= remote_id %>',
-							html: "<iframe height='300' scrolling='no' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'></iframe>",
-							height: 400,
-							width: 800,
-							id: (groups) => groups.join('')
-						},
+							youtube: true,
+							coub: true,
 
-						codepen:
-						{
-							regex: /https?:\/\/codepen.lol\/([^\/\?\&]*)\/pen\/([^\/\?\&]*)/,
-							embedUrl: 'https://codepen.io/<%= remote_id %>?height=300&theme-id=0&default-tab=css,result&embed-version=2',
-							html: "<iframe height='300' scrolling='no' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'></iframe>",
-							height: 300,
-							width: 600,
-							id: (groups) => groups.join('/embed/')
-						},
+							codepen: {
+								regex: /https?:\/\/codepen.io\/([^\/\?\&]*)\/pen\/([^\/\?\&]*)/,
+								embedUrl: 'https://codepen.io/<%= remote_id %>?height=300&theme-id=0&default-tab=css,result&embed-version=2',
+								html: "<iframe height='300' scrolling='no' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'></iframe>",
+								height: 300,
+								width: 600,
+								id: (groups) => groups.join('/embed/')
+							},
+
+							falstad:
+							{
+								regex: /https?:\/\/www.falstad.com\/circuit\/circuitjs.html\?cct=(.*)/gim,
+								embedUrl: 'https://www.falstad.com/circuit/circuitjs.html?cct=<%= remote_id %>',
+								html: "<iframe height='300' scrolling='no' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'></iframe>",
+								height: 400,
+								width: 800,
+								//id: (groups) => groups.join('')
+							},
+						}
 					}
 				},
 				quote: { class: Quote, inlineToolbar: itb },
