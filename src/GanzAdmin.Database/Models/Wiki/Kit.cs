83b29a05c72ef6660aa80c5d86dc90f2ebcf77ef
Szlatyka <kimmelgabor@gmail.com>
2020-07-10 20:11:55 +0200

    Added entities for kits

---
+++ src/GanzAdmin.Database/Models/Wiki/Kit.cs
@@ -0,0 +1,26 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GanzAdmin.Database.Models.Wiki
+{
+    public class Kit : IEntity
+    {
+        [BsonId]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public List<KitPart> Parts { get; set; } = new List<KitPart>();
+
+        public string DisplayValue
+        {
+            get
+            {
+                return this.Name;
+            }
+        }
+    }
+}
---
+++ src/GanzAdmin.Database/Models/Wiki/KitPart.cs
@@ -0,0 +1,15 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GanzAdmin.Database.Models
+{
+    public class KitPart
+    {
+        [BsonRef]
+        public Part Part { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
