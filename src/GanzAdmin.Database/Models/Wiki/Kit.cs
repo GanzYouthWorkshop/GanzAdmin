c117a16a6fe0f2d9396b8fa4514598c0bc0e8611
Szlatyka <kimmelgabor@gmail.com>
2020-07-17 16:32:30 +0200

    Added kit view and amini-tweak on memeber projects

--- src/GanzAdmin.Database/Models/Wiki/Kit.cs
+++ src/GanzAdmin.Database/Models/Wiki/Kit.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace GanzAdmin.Database.Models.Wiki
+namespace GanzAdmin.Database.Models
 {
     public class Kit : IEntity
     {
@@ -14,6 +14,25 @@ namespace GanzAdmin.Database.Models.Wiki
         public string Description { get; set; }
         public string ImageUrl { get; set; }
         public List<KitPart> Parts { get; set; } = new List<KitPart>();
+
+        public bool IsAvailableForBuild
+        {
+            get
+            {
+                bool result = true;
+
+                foreach(KitPart part in this.Parts)
+                {
+                    if(part.Part.SumInStock < part.Amount)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+
+                return result;
+            }
+        }
 
         public string DisplayValue
         {
