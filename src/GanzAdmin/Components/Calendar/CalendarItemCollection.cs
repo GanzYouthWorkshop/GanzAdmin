c556eb1815c09a42f5fbd911e3869379183c9732
Szlatyka <kimmelgabor@gmail.com>
2021-01-10 19:22:23 +0100

    Added mechanism to unversal calendar for payment display.

---
+++ src/GanzAdmin/Components/Calendar/CalendarItemCollection.cs
@@ -0,0 +1,17 @@
+﻿using GanzAdmin.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GanzAdmin.Components.Calendar
+{
+    public class CalendarItemCollection
+    {
+        public Type ConentType { get; set; }
+        public string Color { get; set; }
+        public Func<IEntity, string> DisplayText { get; set; }
+
+        public List<IEvent> CollectedItems { get; set; }
+    }
+}
