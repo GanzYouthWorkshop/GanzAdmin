c0af8567af214a105c192fc955e8b10e402415c4
Gábor Kimmel <gabor.kimmel@apexlogic.eu>
2024-01-20 14:39:11 +0100

    Added OpenGraph metadata to GanzWebpage

---
+++ src/GanzAdmin.Database/Models/Calendar/RepeatingEventPatterns.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GanzAdmin.Database.Models.Calendar
+{
+    [Flags]
+    public enum RepeatingEventPatterns
+    {
+        None = 0,
+        Daily = 1,
+        Weekly = 2,
+        Monthly = 4
+    }
+}
---
+++ src/GanzNet.Common/WysiwygImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GanzNet.Common
+{
+    public class WysiwygImage
+    {
+        public string Url { get; set; }
+        public string Caption { get; set; }
+    }
+}
