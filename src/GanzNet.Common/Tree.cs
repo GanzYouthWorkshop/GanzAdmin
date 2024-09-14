6e00c51ed71cc1306bfe01276d79494b223dd03d
Kimmel Gábor <kimmelgabor@gmail.com>
2024-09-14 11:13:39 +0200

    Added Tree datastructure to common lib

---
+++ src/GanzNet.Common/Tree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GanzNet.Common
+{
+    public class Tree<T>
+    {
+        public T Item { get; set; }
+        public List<Tree<T>> Children { get; set; } = new List<Tree<T>>();
+
+        public bool HasChildren
+        { 
+            get
+            {
+                return this.Children?.Count > 0;
+            }
+        }
+    }
+}
